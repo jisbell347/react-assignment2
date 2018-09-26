7c15e5eb7d76295ae79fb2c55132c73de292e010
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 14:16:26 -0600

    Moved validation.js into seperate directory for better structure

---
+++ lists-conditionals--assignment-problem/src/ValidationComponent/Validation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const validation = (props) => {
+
+	let validationMessage = 'Text too long';
+
+	if(props.inputLength <= 5 ) {
+		validationMessage = 'Text too short';
+	}
+	return (
+		<div>
+			<p>{validationMessage}</p>
+		</div>
+	);
+};
+
+export default validation;
