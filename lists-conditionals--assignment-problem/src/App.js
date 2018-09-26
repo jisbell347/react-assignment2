c28c32e99e61df2fe921f7b6d973a5782c4a2646
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 14:38:20 -0600

    Created a charList variable that maps the userInput text and displays each character in its own box

--- lists-conditionals--assignment-problem/src/App.js
+++ lists-conditionals--assignment-problem/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './ValidationComponent/Validation';
+import Char from  './Char/Char';
+
 class App extends Component {
    state = {
       userInput: ''
@@ -11,6 +13,11 @@ class App extends Component {
    };
 
   render() {
+     const charList = this.state.userInput.split('').map((ch, index) => {
+        return <Char character={ch} key={index} />;
+     });
+
+
     return (
       <div className="App">
         <ol>
@@ -26,6 +33,7 @@ class App extends Component {
          <input type="text" onChange={this.inputChangeHandler} value={this.state.userInput}/>
          <p>{this.state.userInput}</p>
          <Validation inputLength={this.state.userInput.length}/>
+         {charList}
       </div>
     );
   }
